fdbf9da02b6bd38aa87e4d13841adcd006b689af
Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>
2024-07-18 08:39:22 +0530

    folders

---
+++ dist/portfolio-victoriya/browser/chunk-4JCYIGWM.js
@@ -0,0 +1,1 @@
+import{Na as a,Oa as r,X as o,Xa as i,Za as s}from"./chunk-MYJJTMWJ.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-feature"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,f){n&1&&(a(0,"p"),i(1,"feature works!"),r())}});let t=e;return t})();export{m as FeatureComponent};
