20b642a3fa68e88fe8322427693ea7a357837d5d
Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>
2024-07-20 09:13:23 +0530

    Save current progress before updating Angular packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { routes } from './app.routes';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from "./components/about/about.component";
-import { SkillsComponent } from "./components/skills/skills.component";
-import { ProjectsComponent } from "./components/projects/projects.component";
+
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ContactComponent],
+  imports: [RouterOutlet, NavbarComponent, ContactComponent,BrowserAnimationsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  animations: [
+    trigger('iconAnimation', [
+      state('open', style({
+        transform: 'rotate(90deg)',
+        opacity: 1
+      })),
+      state('closed', style({
+        transform: 'rotate(0deg)',
+        opacity: 0.7
+      })),
+      transition('closed <=> open', [
+        animate('300ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class NavbarComponent {
+  isOpen = false;
 
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
 }
