a194f61bf9c9cbd397f24eb9a0c20ffcda14f7de
Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>
2024-07-23 07:58:56 +0530

    ng boostrap

---
+++ dist/portfolio-victoriya/browser/chunk-PXQSNQKE.js
@@ -0,0 +1,1 @@
+import{Ma as a,Na as r,W as o,Wa as i,Ya as s}from"./chunk-TD7KKCT5.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-feature"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,f){n&1&&(a(0,"p"),i(1,"feature works!"),r())}});let t=e;return t})();export{m as FeatureComponent};
